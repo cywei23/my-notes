d53b7b76b87b48a96036bc7bf580175dd5f36f7f
Chun-Yang Wei <cywei23@yahoo.com.tw>
2018-09-14 11:43:04 -0400

    Move miscellaneous functions to useful_func

--- python/Modeler.py
+++ python/Modeler.py
@@ -488,30 +488,3 @@ def profile(fit, x_test, rank_count = 10):
     decile_profile = scored.groupby('rank').mean()
     
     return decile_profile
-
-################## OTHER MISCELLANEOUS FUNCTIONS#############################
-def rsquared(x, y):
-    # Return R^2 where x and y are array-like
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    return r_value**2
-    
-def proc_freq(df, Class, Val):
-    total_count = df.shape[0]
-    count = df.groupby(Class)[Val].count()
-    mix = df.groupby(Class)[Val].count() / df.shape[0]
-    foo = pd.concat([count.rename("Count"), mix.rename("Mix")], axis = 1)
-    bar = pd.DataFrame([[total_count, 1]], columns = ['Count', 'Mix'], index = ['Total'])
-    
-    print pd.concat([foo, bar], axis = 0)
-    
-def proc_means(df, Class, Val):
-    total_count = df.shape[0]
-    total_sum = df[Val].sum()
-    total_mean = df[Val].mean()
-    count = df.groupby(Class)[Val].count()
-    Sum = df.groupby(Class)[Val].sum()
-    Mean = df.groupby(Class)[Val].mean()
-    foo = pd.concat([count.rename("Count"), Sum.rename("Sum"), Mean.rename("Mean")], axis = 1)
-    bar = pd.DataFrame([[total_count, total_sum, total_mean]], columns = ['Count', 'Sum', 'Mean'], index = ['Total'])
-    
-    print pd.concat([foo, bar], axis = 0)
