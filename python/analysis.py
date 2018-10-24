4630f43cf79a0798ca4bf2d21d5602219ed3a9de
Chun-Yang Wei <cywei23@yahoo.com.tw>
2018-10-24 13:39:33 -0400

    Update analysis.py

--- python/analysis.py
+++ python/analysis.py
@@ -1,3 +1,5 @@
+#### THIS IS JUST PSEUDO CODE!!! ####
+
 # Find out cardinality on all columns and then one-hot encoding
 cardinality = [[col, len(df[col].unique())] for col in df.columns]
 cardinality = pd.DataFrame(cardinality)
@@ -18,3 +20,38 @@ df.col.str.contains()
 
 df.describe(include = ['O']) # non-numeric columns only
 df.describe(include = 'all') # all columns
+
+# map method with dictionary to change value
+mapping_dict = {
+    'Android': 'Android',
+    'Chrome OS': 'Chrome OS',
+    'Linux': 'Linux',
+    'Mac OS': 'macOS',
+    'No OS': 'No OS',
+    'Windows': 'Windows',
+    'macOS': 'macOS'
+}
+laptops['os'] = laptops['os'].map(mapping_dict)
+
+# cleaning data with str.split(n=1, expand=True), strip() to get rid of leading and trailing space
+for i in ['storage_1','storage_2']:
+    s_capacity = i+'_capacity_gb'
+    s_type = i+'_type'
+    laptops[[s_capacity,s_type]]=laptops[i].str.split(n=1,expand=True)
+    laptops[s_capacity]=laptops[s_capacity].astype(float)
+    laptops[s_type]=laptops[s_type].str.strip()
+laptops.drop(['storage','storage_1','storage_2'],axis=1,inplace=True)
+
+# rename column
+laptops.rename({"screen_size": "screen_size_inches"}, axis=1, inplace=True)
+
+# use function and list comprehension to clean column names
+def clean_col(s):
+    s = s.strip()
+    s = s.replace('Operating System','os')
+    s = s.replace(' ','_')
+    s = s.replace('(','')
+    s = s.replace(')','')
+    s = s.lower()
+    return s    
+laptops.columns = [clean_col(i) for i in laptops.columns]
