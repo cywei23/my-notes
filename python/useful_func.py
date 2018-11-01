d606805e5dd65ada514f08538a2c46f90cd3c6d0
Chun-Yang Wei <cywei23@yahoo.com.tw>
2018-11-01 11:30:44 -0400

    Add compare dataframe function

--- python/useful_func.py
+++ python/useful_func.py
@@ -38,3 +38,21 @@ def rawincount(filename):
     f = open(filename, 'rb')
     bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
     return sum( buf.count(b'\n') for buf in bufgen )
+
+# Compare two dataframe
+import pandas as pd
+import numpy as np
+
+def compare_two_dfs(input_df_1, input_df_2):
+    df_1, df_2 = input_df_1.copy(), input_df_2.copy()
+    ne_stacked = (df_1 != df_2).stack()
+    changed = ne_stacked[ne_stacked]
+    changed.index.names = ['id', 'col']
+    difference_locations = np.where(df_1 != df_2)
+
+    changed_from = df_1.values[difference_locations]
+
+    changed_to = df_2.values[difference_locations]
+
+    df = pd.DataFrame({'from': changed_from, 'to': changed_to}, index=changed.index)
+    return df
