98f45bfe9051300f9dc60042328a1e72b85ff62d
Chun-Yang Wei <cywei23@yahoo.com.tw>
2018-11-11 07:24:47 -0500

    Update magic_command.py

--- python/magic_command.py
+++ python/magic_command.py
@@ -11,7 +11,12 @@
 %autoreload 2
 
 %who_ls    #show used name
+%who
 %who_ls function
 
 %load_ext line_profiler
 %lprun
+
+%run file.py           #run external python file
+
+!ls -l     #use ! to access command line call
