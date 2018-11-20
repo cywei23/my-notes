646347d7a9465efdb57e10d7c5be8ae1deeab12c
Chun-Yang Wei <cywei23@yahoo.com.tw>
2018-11-20 04:47:17 -0500

    Heatmap code

--- python/visualization.py
+++ python/visualization.py
@@ -10,3 +10,18 @@ for i in cols:
     g.add_legend()
     g.fig.suptitle(i)
     plt.show()
+
+# Heatmap for comparing model decile distribution
+## create list permutation first
+import itertools
+perm = list(itertools.permutations(hundreds_col, 2))
+
+## create heatmap
+for i in perm:
+    bnsr_gb = bnsr.groupby(list(i), as_index=False).agg({'consumer_id':'count'})
+    bnsr_gb['consumer_id'] = bnsr_gb['consumer_id']/488862*100
+    bnsr_gb = bnsr_gb.pivot(list(i)[0], list(i)[1], 'consumer_id')
+    ax = sns.heatmap(bnsr_gb, annot=True, fmt='.1f', cmap="YlGnBu")
+    ax.invert_yaxis()
+    plt.show()
+
